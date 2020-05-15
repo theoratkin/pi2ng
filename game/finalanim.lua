6954726e2baaae9dee91478bc8fac109fc6fc54d
Theogen Ratkin <feogenratkin *at* gmail.com>
2020-05-15 13:43:24 -0400

    Use easing function for Sun animation

--- game/finalanim.lua
+++ game/finalanim.lua
@@ -2,17 +2,20 @@ Object = require "classic"
 local FinalAnim = Object:extend()
 
 
+local SUNSTART = 3
 local SUNSPEED = 100
 local SUNSIZE = 150
 local CIRCLES_SPEED = 50
 local CIRCLES_COUNT = 15
+local DUMMY_CIRCLES = 4
 
 
 function FinalAnim:new()
 	self.active = false
 	self.sunColor = {.8, .7, 0}
 
-	self.sun = 3
+	self.sunlinear = SUNSTART
+	self.sunease = 0
 	self.circles = {}
 	for i = 1, CIRCLES_COUNT do
 		self.circles[i] = 1
@@ -33,18 +36,19 @@ function FinalAnim:update(dt)
 	end
 
 	local diag = math.sqrt(size.x * size.x - size.y * size.y) / 2
-	if self.circles[3] and self.circles[3] > diag * 2 then
+	if self.circles[DUMMY_CIRCLES] and self.circles[DUMMY_CIRCLES] > diag * 2 then
 		for i in pairs(self.circles) do
 			self.circles[i] = nil
 		end
-	elseif self.circles[3] then
-		for i = 3, CIRCLES_COUNT do
+	elseif self.circles[DUMMY_CIRCLES] then
+		for i = DUMMY_CIRCLES, CIRCLES_COUNT do
 			self.circles[i] = self.circles[i] + i * dt * CIRCLES_SPEED
 		end
 	end
 
-	if self.sun < SUNSIZE then
-		self.sun = self.sun + SUNSPEED * dt
+	if self.sunlinear < SUNSIZE then
+		self.sunlinear = self.sunlinear + SUNSPEED * dt
+		self.sunease = SUNSIZE * easeOutCubic(self.sunlinear / SUNSIZE)
 	end
 end
 
@@ -67,9 +71,13 @@ function FinalAnim:draw()
 	love.graphics.circle(
 		"fill",
 		center.x, center.y,
-		self.sun
+		self.sunease
 	)
 end
 
+
+function easeOutCubic(x)
+	return 1 - (1 - x) ^ 3
+end
 
 return FinalAnim
